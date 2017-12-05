10111eccbc790af895253e570c46d52fe43e23ac
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-12-04 20:50:38 -0400

    add test cases

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import unittest
 from utils import *
+from main import move_up, move_down, move_left, move_right
+from enemy import *
 
 
 class TestSuite(unittest.TestCase):
@@ -93,6 +95,64 @@ class TestSuite(unittest.TestCase):
         ]
         self.assertEqual(check_diagonals(win_state), 'O')
         self.assertEqual(check_diagonals(no_win_state), ' ')
+
+    def test_move_up(self):
+        top_row_pos = 0
+        bottom_row_pos = 8
+        self.assertEqual(move_up(top_row_pos), 0)
+        self.assertEqual(move_up(bottom_row_pos), 5)
+
+    def test_move_down(self):
+        top_row_pos = 0
+        bottom_row_pos = 8
+        self.assertEqual(move_down(top_row_pos), 3)
+        self.assertEqual(move_down(bottom_row_pos), 8)
+        
+    def test_move_left(self):
+        left_col_pos = 0
+        right_col_pos = 8
+        self.assertEqual(move_left(left_col_pos), 0)
+        self.assertEqual(move_left(right_col_pos), 7)
+        
+    def test_move_right(self):
+        left_col_pos = 0
+        right_col_pos = 8
+        self.assertEqual(move_right(left_col_pos), 1)
+        self.assertEqual(move_right(right_col_pos), 8)
+
+    def test_get_move(self):
+        win_opportunity = [
+            ['O', ' ', ' '],
+            ['O', 'O', ' '],
+            ['X', ' ', 'X'],
+        ]
+        block_opportunity = [
+            ['O', ' ', ' '],
+            ['O', 'X', ' '],
+            [' ', ' ', ' '],
+        ]
+        center_opportunity = [
+            ['O', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+        ]
+        self.assertEqual(get_move(win_opportunity), [2, 1])
+        self.assertEqual(get_move(block_opportunity), [2, 0])
+        self.assertEqual(get_move(center_opportunity), [1, 1])
+
+    def test_count_win_moves(self):
+        win_opportunity = [
+            ['O', ' ', ' '],
+            ['O', 'O', ' '],
+            ['X', ' ', 'X'],
+        ]
+        multi_win_opportunity = [
+            ['O', 'O', 'X'],
+            ['O', 'O', ' '],
+            ['X', ' ', 'X'],
+        ]
+        self.assertEqual(count_win_moves(win_opportunity), 1)
+        self.assertEqual(count_win_moves(multi_win_opportunity), 2)
 
 
 if __name__ == '__main__':
